e64970290f8d8996777925f03f3365285c90430a
bhn0108 <bhn0108@icloud.com>
2023-06-11 21:38:11 +0900

    crate base folder and file

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <h1><a href="#">TODO-List</a></h1>
+      <nav>
+        <ul>
+            <li>メニュー1</li>
+            <li>メニュー2</li>
+            <li>メニュー3</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
